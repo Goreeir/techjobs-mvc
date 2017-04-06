4fbca127b2443e0cde39b3592bf044aba78afbcf
Ieshea Goree <iesheagoree@gmail.com>
2017-04-06 18:29:28 -0500

    Worded on my Todos

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -24,4 +24,17 @@ public class SearchController {
 
     // TODO #1 - Create handler to process search request and display results
 
+    @RequestMapping(value = "results")
+    public  String search(@RequestParam String searchTerm, @RequestParam String searchType, Model model) {
+        model.addAttribute("columns", ListController.columnChoices);
+        ArrayList<HashMap<String, String>> jobSearch;
+            if (searchType.equals("all")) {
+                jobSearch = JobData.findByValue(searchTerm);
+            }
+            else{
+                jobSearch = JobData.findByColumnAndValue(searchType, searchTerm);
+                }
+        model.addAttribute("results", jobSearch);
+        return "search";
+    }
 }
